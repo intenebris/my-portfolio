cda89d1f4eba177c645fb931fd43cb29f9fa1c2d
Michael <69639434+intenebris@users.noreply.github.com>
2020-09-27 12:38:43 +0300

    Перезалив портволио

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.slider-wrapper').slick({
+        infinite: true,
+        speed: 500,
+        autoplay: true,
+        autoplaySpeed: 5000,
+        fade: true,
+        cssEase: 'linear'
+    });
+})
